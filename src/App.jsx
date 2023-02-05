8b2dfc302263d2f4facdb84e76e8ff505f57ecbe
AnaisRevellat <anaisrevellat@gmail.com>
2023-02-05 12:26:48 +0100

    css

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./global.css";
+import s from './style.module.css';
+
+
+export function App(){
+  return( 
+  <div className={s.main_container}>
+    <div className={s.header}>Header</div>
+    <div className={s.tv_show_detail}>Detail</div>
+    <div className={s.recommendations}>Recommendations</div>
+  </div>
+  )
+}
